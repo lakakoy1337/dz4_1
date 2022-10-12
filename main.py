9123abcdbb3060a063dfb45b0bdb7808dc689b76
lakakoy1337 <zaqw1151@gmail.com>
2022-10-12 16:07:11 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+
+print("1 число:")
+A = int(input())
+print("2 число:")
+B = int(input())
+print("3 число:")
+C = int(input())
+
+def minimum(A, B, C):
+    return min(A, B, C)
+
+print("Самое мелкое: ", minimum(A, B, C))
